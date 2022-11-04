eadabdc240f7de0c89a5dde3c3ec87fcbaf2a4cc
Astakhov <astakhov24rus@yandex.ru>
2022-11-04 20:03:28 +0700

    brain step 5. Release Even game.

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import prompt
+from random import randint
+
+
+def main():
+    name = prompt.string('May I have your name? ')
+    print ('Hello, ' + name + '!\nAnswer "yes" if the number is even, otherwise answer "no".')
+    count = 0
+    while 0 <= count <= 2:
+        x = randint(1,100)
+        question = print ('Question: '+ str(x))
+        answer = prompt.string('Your answer: ')
+        if x % 2 == 0:
+            if answer == 'yes':
+                count += 1
+                print ('Correct!')
+            else:
+                count == 0
+                print ('\'' + answer + '\' is wrong answer ;(. Correct answer was \'yes\'.\nLet\'s try again,' + name +'!')
+                break
+        elif x % 2 != 0:
+            if answer == 'no':
+                count += 1
+                print ('Correct!')
+            else:
+                count == -1
+                print ('\'' + answer + '\' is wrong answer ;(. Correct answer was \'no\'.\nLet\'s try again,' + name +'!')
+                break
+    if count == 3:
+        print ('Congratulations, ' + name + '!')
+
+
+if __name__ == '__main__':
+    main()
