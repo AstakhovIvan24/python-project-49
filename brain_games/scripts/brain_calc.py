2c4a7cb52c7eddc99ee8c6c7f7ae1facfef031c8
Astakhov <astakhov24rus@yandex.ru>
2022-11-08 12:22:48 +0700

    Brain step 8. Released Brain-progression.

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -6,50 +6,51 @@ from random import choice
 
 
 def main():
-    print ('Welcome to the Brain Games!')
+    print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
-    print ('Hello, ' + name + '!\nWhat is the result of the expression?')
+    print('Hello, ' + name + '!\nWhat is the result of the expression?')
     count = 0
     while 0 <= count <= 2:
-        x = randint(0,100)
-        y = randint(0,100)
-        op = ['-','+','*']
-        operation = {'-':(x-y), '+':(x+y),'*':(x*y)}
+        x = randint(0, 100)
+        y = randint(0, 100)
+        op = ['-', '+', '*']
+        operation = {'-': (x - y), '+': (x + y), '*': (x * y)}
         make_choice = choice(op)
-        question = print ('Question: '+ str(x) + make_choice + str(y))
+        print('Question: ' + str(x) + make_choice + str(y))
         answer = prompt.string('Your answer: ')
         if make_choice == '-':
             if answer == str(x - y):
                 count += 1
-                print ('Correct!')
+                print('Correct!')
             else:
-                count == 0
                 result = str(x - y)
                 return wrong(answer, result, name)
         elif make_choice == '+':
             if answer == str(x + y):
                 count += 1
-                print ('Correct!')
+                print('Correct!')
             else:
-                count == 0
                 result = str(x + y)
                 return wrong(answer, result, name)
         elif make_choice == '*':
             if answer == str(x * y):
                 count += 1
-                print ('Correct!')
+                print('Correct!')
             else:
-                count == 0
                 result = str(x * y)
                 return wrong(answer, result, name)
         else:
             result = str(operation[make_choice])
             return wrong(answer, result, name)
     if count == 3:
-        print ('Congratulations, ' + name + '!')
+        print('Congratulations, ' + name + '!')
+
 
 def wrong(answer, result, name):
-    return print ('\'' + answer + '\' is wrong answer ;(. Correct answer was \'' + result +'\'.\nLet\'s try again,' + name +'!')
+    return print(f"'{answer}' is wrong answer ;(."
+                 f"Correct answer was '{result}'.\n"
+                 f"Let\'s try again,{name}!")
+
 
 if __name__ == '__main__':
     main()
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -5,33 +5,38 @@ from random import randint
 
 
 def main():
-    print ('Welcome to the Brain Games!')
+    print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
-    print ('Hello, ' + name + '!\nAnswer "yes" if the number is even, otherwise answer "no".')
+    print(f"Hello, {name}!\n"
+          f"Answer 'yes' if the number is even, otherwise answer 'no'.")
     count = 0
     while 0 <= count <= 2:
-        x = randint(1,100)
-        question = print ('Question: '+ str(x))
+        x = randint(1, 100)
+        print('Question: ' + str(x))
         answer = prompt.string('Your answer: ')
         if x % 2 == 0:
             if answer == 'yes':
                 count += 1
-                print ('Correct!')
+                print('Correct!')
             else:
                 result = 'yes'
                 return wrong(answer, result, name)
         elif x % 2 != 0:
             if answer == 'no':
                 count += 1
-                print ('Correct!')
+                print('Correct!')
             else:
                 result = 'no'
                 return wrong(answer, result, name)
     if count == 3:
-        print ('Congratulations, ' + name + '!')
+        print('Congratulations, ' + name + '!')
+
 
 def wrong(answer, result, name):
-    return print ('\'' + answer + '\' is wrong answer ;(. Correct answer was \'' + result +'\'.\nLet\'s try again,' + name +'!')
+    return print(f"'{answer}' is wrong answer ;(."
+                 f"Correct answer was '{result}'.\n"
+                 f"Let\'s try again,{name}!")
+
 
 if __name__ == '__main__':
     main()
