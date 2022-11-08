1a16edc9d45abec30a6765ed12a410d2c2404379
Astakhov <astakhov24rus@yandex.ru>
2022-11-08 12:53:09 +0700

    Brain step 9. Released Brain-prime.

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Brain-even link: https://asciinema.org/a/zdIDsrEd4b47no430WUAxyR3L
 Brain-calc link: https://asciinema.org/a/J1OPW46gGInaMkkBElzPXV8xM
 Brain-gcd link: https://asciinema.org/a/qGqLBKFgssQPJZyPZkHZW0nnt
 Brain-progression link: https://asciinema.org/a/hWVzmXwfCfdXKgOVmgd1IkuU5
+Brain-prime link: https://asciinema.org/a/ePgzUPhUyc9I2uIloh5mI5luN
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import prompt
+from random import randint
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f"Hello, {name}!\n"
+          f"Answer 'yes' if given number is prime. Otherwise answer 'no'.")
+    count = 0
+    while 0 <= count < 3:
+        x = randint(0, 100)
+        y = prime(x)
+        print('Question: ', x)
+        answer = prompt.string('Your answer: ')
+        if answer == y:
+            count += 1
+            print('Correct!')
+        else:
+            result = y
+            wrong(answer, result, name)
+    if count == 3:
+        print('Congratulations, ' + name + '!')
+
+
+def prime(x):
+    k = 0
+    for i in range(2, x // 2 + 1):
+        if x % i == 0:
+            k = k + 1
+    if k <= 0:
+        return 'yes'
+    else:
+        return 'no'
+
+
+def wrong(answer, result, name):
+    print(f"'{answer}' is wrong answer ;(."
+          f"Correct answer was '{result}'.\n"
+          f"Let\'s try again,{name}!")
+
+
+if __name__ == '__main__':
+    main()
